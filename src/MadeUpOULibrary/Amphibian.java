1fb4bf875183fa2416c66b70f224d6e1aa396d0d
Mark Standen <mark@markstanden.dev>
2022-06-05 14:27:54 +0100

    edits to madeUpOULibrary classes

--- src/MadeUpOULibrary/Amphibian.java
+++ src/MadeUpOULibrary/Amphibian.java
@@ -4,18 +4,27 @@ import java.awt.*;
 
 public class Amphibian
 {
+	public static int MAX_STONES = 10;
+
+	protected int position;
+
+	private Color colour;
+
+
+
 	public Color getColour()
 	{
 		return new Color(colour.getRGB());
 	}
 
 
-	private Color colour;
-
-
-	public Amphibian()
+	public Amphibian(int initialPos) throws IllegalArgumentException
 	{
 		colour = Color.DARK_GRAY;
+		if(initialPos > MAX_STONES || initialPos < 1) {
+			throw new IllegalArgumentException("Invalid starting location.");
+		}
+		position = initialPos;
 	}
 
 
@@ -23,4 +32,52 @@ public class Amphibian
 	{
 		colour = aColour;
 	}
+
+
+	public void setPosition(int aPosition)
+	{
+		position = aPosition;
+	}
+
+
+	/**
+	 * Returns the current position of the amphibian
+	 *
+	 * @return the int representing the stone the amphibian is currently on
+	 */
+	public int getPosition()
+	{
+		return position;
+	}
+
+
+	/**
+	 * Moves the amphibian left by the number of supplied stones
+	 *
+	 * @param numberOfStones
+	 * 		The number of stones to move.
+	 */
+	public void left(int numberOfStones)
+	{
+		if(position > 1) {
+			setPosition(position - numberOfStones);
+			System.out.printf("Left %d\n", numberOfStones);
+		}
+	}
+
+
+	/**
+	 * Moves the amphibian right by the number of supplied stones
+	 *
+	 * @param numberOfStones
+	 * 		The number of stones to move.
+	 */
+	public void right(int numberOfStones)
+	{
+		if(position < MAX_STONES) {
+			setPosition(position + numberOfStones);
+			System.out.printf("Right %d\n", numberOfStones);
+		}
+	}
+
 }
--- src/MadeUpOULibrary/Frog.java
+++ src/MadeUpOULibrary/Frog.java
@@ -2,9 +2,10 @@ package MadeUpOULibrary;
 
 public abstract class Frog extends Amphibian
 {
-	public static int MAX_STONES = 10;
-
-	protected int position;
+	public Frog(int initialPos) throws IllegalArgumentException
+	{
+		super(initialPos);
+	}
 
 
 	public Frog()
@@ -13,49 +14,9 @@ public abstract class Frog extends Amphibian
 	}
 
 
-	public Frog(int initialPos) throws IllegalArgumentException
-	{
-		super();
-		if(initialPos > MAX_STONES || initialPos < 1) {
-			throw new IllegalArgumentException("Invalid starting location.");
-		}
-		position = initialPos;
-	}
-
-
-	public void setPosition(int aPosition)
-	{
-		position = aPosition;
-	}
-
-
-	public int getPosition()
-	{
-		return position;
-	}
-
-
 	public void jump()
 	{
 		System.out.println("Jump");
-	}
-
-
-	public void left(int numberOfStones)
-	{
-		if(position > 1) {
-			setPosition(position - numberOfStones);
-			System.out.printf("Left %d\n", numberOfStones);
-		}
-	}
-
-
-	public void right(int numberOfStones)
-	{
-		if(position < MAX_STONES) {
-			setPosition(position + numberOfStones);
-			System.out.printf("Right %d\n", numberOfStones);
-		}
 	}
 
 
